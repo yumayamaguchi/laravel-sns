30bafe4bf63b6d1ddca1fb0a2a93a019fcbe6a28
yumayamaguchi <80366714+yumayamaguchi@users.noreply.github.com>
2021-06-08 20:07:39 +0900

    記事投稿機能の作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,9 +28,12 @@
                                     <input class="form-control" type="password" id="password" name="password" required>
                                 </div>
 
-                                {{-- ここから --}}
+
                                 <input type="hidden" name="remember" id="remember" value="on">
-                                {{-- ここまで --}}
+
+                                <div class="text-left">
+                                    <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                                </div>
 
                                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 
